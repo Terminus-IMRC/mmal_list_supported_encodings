706cc9e28b62a82b3630cd15aaccc2fce0db0a0e
Terminus <i.can.speak.c.and.basic@gmail.com>
2017-03-23 22:02:18 +0900

    Remove unneeded headers

--- mmal_list_supported_encodings.c
+++ mmal_list_supported_encodings.c
@@ -10,12 +10,9 @@
 #include <interface/mmal/mmal.h>
 #include <interface/mmal/mmal_logging.h>
 #include <interface/mmal/util/mmal_util.h>
-#include <interface/mmal/util/mmal_util_params.h>
-#include <interface/mmal/util/mmal_default_components.h>
 #include <bcm_host.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
 
 
 #define _check(x) do { \
