bbb080dba6c6ed0c08d4f566d1f01a93e151e758
Sugizaki Yukimasa <ysugi@idein.jp>
2018-02-18 01:51:50 +0900

    list_all.sh: Print table of contents

--- list_all.sh
+++ list_all.sh
@@ -16,15 +16,36 @@ echo "# mmal_list_supported_encodings result"
 echo
 echo "https://github.com/Terminus-IMRC/mmal_list_supported_encodings"
 echo
+echo '## `vcgencmd version`'
 echo '```'
 vcgencmd version
 echo '```'
 
-< "$LIST" tr -d ' \t' | grep -v '^#' | grep -v '^$' | while read COMP; do
+echo
+echo "## Table of contents"
+< "$LIST" tr -d ' \t' | grep -v '^$' | while read COMP; do
+	if echo "$COMP" | grep -q '^#'; then
+		name=$(echo "$COMP" | sed 's/^#[ \t]*//')
+		id=$(echo "$name" | tr '[:upper:]' '[:lower:]')
+		echo "- [$name](#$id)"
+		i=1
+	else
+		id=$(echo "$COMP" | tr -d '\.')
+		echo -e "\t- [$COMP](#$id)"
+	fi
+done
+
+< "$LIST" tr -d ' \t' | grep -v '^$' | while read COMP; do
 	echo
-	echo "## $COMP"
-	"$BIN" "$COMP" 2>/dev/null
-	if [ $? -ne 0 ]; then
-		echo "Failed."
+	if echo "$COMP" | grep -q '^#'; then
+		name=$(echo "$COMP" | sed 's/^#[ \t]*//')
+		echo "## $name"
+		i=1
+	else
+		echo "### $COMP"
+		"$BIN" "$COMP" 2>/dev/null
+		if [ $? -ne 0 ]; then
+			echo "Failed."
+		fi
 	fi
 done
